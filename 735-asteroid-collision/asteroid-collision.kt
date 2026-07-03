f7e650024efaecd9d17f8805a1c0622779feb1ad
Ho Yeon CHAE <102277048+leonroars@users.noreply.github.com>
2026-07-03 11:29:36 +0900

    Time: 14 ms (72.73%) | Memory: 47.7 MB (75.32%) - LeetSync

---
+++ 735-asteroid-collision/asteroid-collision.kt
@@ -0,0 +1,42 @@
+// Solution : Using Stack - Compare current result with the one at the top of stack before pushing it.
+
+class Solution {
+    fun asteroidCollision(asteroids: IntArray): IntArray {
+        var locator = 0
+        val stack = Stack<Int>()
+
+        while(locator < asteroids.size){
+            // Case I : Stack is empty -> Push
+            if(stack.isEmpty()){stack.push(asteroids[locator++])}
+
+            // Case II : Not Empty -> Compare
+            else {
+                // While collision exist -> Keep compare
+                var tmp: Int? = asteroids[locator] // `null` can be assigned, by type inference.
+
+                while(tmp != null && doTheyCollide(stack.peek(), tmp)){
+                    tmp = simulateCollision(stack.pop(), tmp)
+                    
+                    if(tmp == null || stack.isEmpty()){break}
+                }
+
+                if(tmp != null){stack.push(tmp)}
+                locator++
+            }
+
+        }
+
+        return stack.toIntArray()
+    }
+
+    fun doTheyCollide(left: Int, right: Int): Boolean {
+        if(left > 0 && right < 0){return true}
+        return false
+    }
+
+    fun simulateCollision(left: Int, right: Int) : Int? {
+        if(Math.abs(left) < Math.abs(right)) {return right}
+        else if(Math.abs(left) > Math.abs(right)) {return left}
+        return null
+    }
+}
